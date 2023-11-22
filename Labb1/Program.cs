2c497d6d69ddd1feb920fcd9fb403e98fea076a8
AhmadYazbek <150136027+AhmadYazbek@users.noreply.github.com>
2023-11-22 08:27:37 +0100

    Add files via upload

---
+++ Labb1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace GIK299_Ahmad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ange namn på familjemedlem 1:");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("ange ålder på familjemedlem 1:");
+            int age1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ange namn på familjemedlem 2:");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("ange ålder på familjemedlem 2:");
+            int age2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ange namn på familjemedlem 3:");
+            string name3 = Console.ReadLine();
+            Console.WriteLine("ange ålder på familjemedlem 3:");
+            int age3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ange namn på familjemedlem 4:");
+            string name4 = Console.ReadLine();
+            Console.WriteLine("ange ålder på familjemedlem 4:");
+            int age4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(name1 + " är " + age1 + " år gammal. ");
+            Console.WriteLine(name2 + " är " + age2 + " år gammal. ");
+            Console.WriteLine(name3 + " är " + age3 + " år gammal. ");
+            Console.WriteLine(name4 + " är " + age4 + " år gammal. ");
+
+            double totalAge = age1 + age2 + age3 + age4;
+            double averageAge = totalAge / 4;
+            Console.WriteLine("totalålder " + "är: " + totalAge);
+            Console.WriteLine("medelålder " + "är: " + averageAge.ToString("0.00"));  
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
